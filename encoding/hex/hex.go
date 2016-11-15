0e3cb8b220fa49c1f3d7d71a29ae64818b97e627
ChamPly <champly@outlook.com>
2016-11-15 21:13:52 +0800

    hex

--- encoding/hex/hex.go
+++ encoding/hex/hex.go
@@ -1,2 +1,18 @@
 package hex
 
+import (
+	"encoding/hex"
+)
+
+func Encode(src []byte) string {
+	return hex.EncodeToString(src)
+}
+
+func Decode(src string) (r string, err error) {
+	data, err := hex.DecodeString(src)
+	if err != nil {
+		return
+	}
+	r = string(data)
+	return
+}
