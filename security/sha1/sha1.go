f59a8542c04d1bf53e340e5d69c4c79ae6d4e43c
ChamPly <champly@outlook.com>
2016-11-16 18:00:05 +0800

    sha1

--- security/sha1/sha1.go
+++ security/sha1/sha1.go
@@ -1,2 +1,13 @@
 package sha1
 
+import (
+	"crypto/sha1"
+	"fmt"
+)
+
+func Encrypt(content string) string {
+	h := sha1.New()
+	h.Write([]byte(content))
+	bs := h.Sum(nil)
+	return fmt.Sprintf("%x", bs)
+}
