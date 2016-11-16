0a5ba59cc0ccd38d4530f719f23728bddeab3e9a
ChamPly <champly@outlook.com>
2016-11-16 11:55:32 +0800

    encoding

--- encoding/encoding_test.go
+++ encoding/encoding_test.go
@@ -20,8 +20,31 @@ func TestGetReader(t *testing.T) {
 
 	charset = ""
 	data, err = ioutil.ReadAll(GetReader(input, charset))
+	if err != nil {
+		t.Errorf("测试失败：%v", err)
+	}
 	if bytes.EqualFold(data, []byte(input)) || data == nil {
 		t.Error("GetReader fail")
+	}
+
+	input = "你好"
+	charset = "gbk"
+	data, err = ioutil.ReadAll(GetReader(input, charset))
+	if err != nil {
+		t.Errorf("测试失败：%v", err)
+	}
+	if strings.EqualFold(string(data), input) {
+		t.Errorf("GetReader fail %s to %s", input, string(data))
+	}
+
+	input = "你好"
+	charset = "gb2312"
+	data, err = ioutil.ReadAll(GetReader(input, charset))
+	if err != nil {
+		t.Errorf("测试失败：%v", err)
+	}
+	if strings.EqualFold(string(data), input) {
+		t.Errorf("GetReader fail %s to %s", input, string(data))
 	}
 }
 
@@ -38,15 +61,34 @@ func TestCovert(t *testing.T) {
 	}
 
 	charset = ""
-	data, err := ioutil.ReadAll(GetReader(input, charset))
+	_, err = ioutil.ReadAll(GetReader(input, charset))
 	if err != nil {
 		t.Errorf("测试失败:%v", err)
 	}
 	actual, err = Convert([]byte(input), charset)
+	if err == nil {
+		t.Error("Covert fail")
+	}
+
+	input = "你好"
+	charset = "gbk"
+	except = "浣犲ソ"
+	actual, err = Convert([]byte(input), charset)
 	if err != nil {
 		t.Error("Covert fail")
 	}
-	if !strings.EqualFold(actual, string(data)) {
-		t.Errorf("GetReader fail %s to %s", string(data), actual)
+	if !strings.EqualFold(actual, except) {
+		t.Errorf("GetReader fail %s to %s", input, actual)
+	}
+
+	input = "你好"
+	charset = "gb2312"
+	except = "浣犲ソ"
+	actual, err = Convert([]byte(input), charset)
+	if err != nil {
+		t.Error("Covert fail")
+	}
+	if !strings.EqualFold(actual, except) {
+		t.Errorf("GetReader fail %s to %s", input, actual)
 	}
 }
