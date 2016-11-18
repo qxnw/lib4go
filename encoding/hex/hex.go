9c5b70490e19c74ed2816d8ec2f7fbfb67fb469a
ChamPly <champly@outlook.com>
2016-11-18 14:18:04 +0800

    hex

--- encoding/hex/hex.go
+++ encoding/hex/hex.go
@@ -4,10 +4,12 @@ import (
 	"encoding/hex"
 )
 
+// 把[]byte类型通过hex编码成string
 func Encode(src []byte) string {
 	return hex.EncodeToString(src)
 }
 
+// 把一个string类型通过hex解码成string
 func Decode(src string) (r string, err error) {
 	data, err := hex.DecodeString(src)
 	if err != nil {
