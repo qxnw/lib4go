736323b3d138f8546c800e279e2cef65dfd65cb8
ChamPly <champly@outlook.com>
2016-11-18 14:38:16 +0800

    url编码和解码

--- encoding/url/url.go
+++ encoding/url/url.go
@@ -4,12 +4,12 @@ import (
 	"net/url"
 )
 
-// 对字符串进行url编码
+// Encode 对字符串进行url编码
 func Encode(input string) string {
 	return url.QueryEscape(input)
 }
 
-// 对字符串进行url解码
+// Decode 对字符串进行url解码
 func Decode(input string) (string, error) {
 	return url.QueryUnescape(input)
 }
