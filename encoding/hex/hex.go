b77608e5844e97e164b6b7323e770d293908eb2f
ChamPly <champly@outlook.com>
2016-11-18 14:37:11 +0800

    hex编码和解码

--- encoding/hex/hex.go
+++ encoding/hex/hex.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 )
 
-// 把[]byte类型通过hex编码成string
+// Encode 把[]byte类型通过hex编码成string
 func Encode(src []byte) string {
 	return hex.EncodeToString(src)
 }
 
-// 把一个string类型通过hex解码成string
+// Decode 把一个string类型通过hex解码成string
 func Decode(src string) (r string, err error) {
 	data, err := hex.DecodeString(src)
 	if err != nil {
