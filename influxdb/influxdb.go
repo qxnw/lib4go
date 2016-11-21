81a3d25b94585720e59ae0531c4ee5f21d5fa8ca
ChamPly <champly@outlook.com>
2016-11-21 18:32:19 +0800

    influxdb存数据

--- influxdb/influxdb.go
+++ influxdb/influxdb.go
@@ -10,6 +10,7 @@ import (
 	"github.com/qxnw/lib4go/utility"
 )
 
+// influxDbConfig influxdb配置
 type influxDbConfig struct {
 	Address   string `json:"address"`
 	DbName    string `json:"db"`
@@ -17,17 +18,20 @@ type influxDbConfig struct {
 	Password  string `json:"password"`
 	RowFormat string `json:"row"`
 }
+
+// InfluxDB 上下文
 type InfluxDB struct {
 	config *influxDbConfig
 }
 
+// New 新建一个influxdb的环境
 func New(config string) (i *InfluxDB, err error) {
 	fmt.Println("new influxdb:", config)
 	i = &InfluxDB{}
 	i.config = &influxDbConfig{}
 	err = json.Unmarshal([]byte(config), &i.config)
 	if err != nil {
-		return
+		return nil, fmt.Errorf("influxdb 配置文件有误:%v", err)
 	}
 	if strings.EqualFold(i.config.Address, "") ||
 		strings.EqualFold(i.config.DbName, "") ||
@@ -37,15 +41,18 @@ func New(config string) (i *InfluxDB, err error) {
 	}
 	return
 }
+
+// SaveString 保存json格式的字符串
 func (db *InfluxDB) SaveString(rows string) (err error) {
 	var data []map[string]interface{}
 	err = json.Unmarshal([]byte(rows), &data)
 	if err != nil {
-		return
+		return fmt.Errorf("influxdb SaveString 反序列化字符串失败:%v", err)
 	}
 	return db.Save(data)
 }
 
+// Save 保存map类型的数据
 func (db *InfluxDB) Save(rows []map[string]interface{}) (err error) {
 	url := fmt.Sprintf("%s/write?db=%s", db.config.Address, db.config.DbName)
 	var datas []string
@@ -56,12 +63,13 @@ func (db *InfluxDB) Save(rows []map[string]interface{}) (err error) {
 	data := strings.Join(datas, "\n")
 	resp, err := http.Post(url, "application/x-www-form-urlencoded", strings.NewReader(data))
 	if err != nil {
-		return
+		return fmt.Errorf("influxdb Post fail:%v", err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode == 204 {
 		return nil
 	}
-	err = errors.New(fmt.Sprintf("influxdb save error:%d", resp.StatusCode))
+	// err = errors.New(fmt.Sprintf("influxdb save error:%d", resp.StatusCode))
+	err = fmt.Errorf("influxdb save error:%d", resp.StatusCode)
 	return
 }
---
+++ influxdb/influxdb_test.go
@@ -0,0 +1,171 @@
+/*
+{
+    "address":"http://192.168.101.183:8086",
+     "db":"influxdb_test",
+     "user":"admin",
+     "password":"123456",
+     "row":"test_table,name=@name,address=@address age=@age"
+}
+*/
+package influxdb
+
+import "testing"
+
+func TestNew(t *testing.T) {
+	// 参数正确
+	config := `{
+    "address":"http://192.168.0.185:8086",
+    "db":"influxdb_test",
+    "user":"admin",
+    "password":"123456",
+    "row":"test_table,name=@name,address=@address age=@age"
+}`
+	_, err := New(config)
+	if err != nil {
+		t.Errorf("create influxdb context fail:%v", err)
+	}
+
+	// 缺少参数
+	config = `{
+    "address":"http://192.168.0.185:8086",
+    "user":"admin",
+    "password":"123456",
+    "row":"test_table,name=@name,address=@address age=@age"
+}`
+	_, err = New(config)
+	if err == nil {
+		t.Error("test fail")
+	}
+
+	// 缺少参数
+	config = `{
+    "address":"http://192.168.0.185:8086",
+    "db":"influxdb_test",
+    "row":"test_table,name=@name,address=@address age=@age"
+}`
+	_, err = New(config)
+	if err != nil {
+		t.Errorf("create influxdb context fail:%v", err)
+	}
+
+	// 缺少参数
+	config = `{
+    "address":"http://192.168.0.185:8086",
+    "db":"influxdb_test",
+    "user":"admin",
+    "password":"123456"
+}`
+	_, err = New(config)
+	if err == nil {
+		t.Error("test fail")
+	}
+
+	// 缺少参数
+	config = `
+    "address":"http://192.168.0.185:8086",
+    "db":"influxdb_test",
+    "user":"admin",
+    "password":"123456"
+}`
+	_, err = New(config)
+	if err == nil {
+		t.Error("test fail")
+	}
+}
+
+func TestSaveString(t *testing.T) {
+	// 正常流程
+	config := `{
+    "address":"http://192.168.0.185:8086",
+    "db":"influxdb_test",
+    "user":"admin",
+    "password":"123456",
+    "row":"test_table,name=@name,address=@address age=@age"
+}`
+	i, err := New(config)
+	if err != nil {
+		t.Errorf("create influxdb context fail:%v", err)
+	}
+
+	rows := `[{"name":"champly","address":"china","age":"18"}]`
+	err = i.SaveString(rows)
+	if err != nil {
+		t.Errorf("SaveString fail:%v", err)
+	}
+
+	// 参数错误
+	config = `{
+    "address":"http://192.168.0.185:8086",
+    "db":"influxdb_test",
+    "user":"admin",
+    "password":"123456",
+    "row":"test_table,name=@name,address=@address age=@age"
+}`
+	i, err = New(config)
+	if err != nil {
+		t.Errorf("create influxdb context fail:%v", err)
+	}
+
+	rows = `[{"name":"champly","address":"china"}]`
+	err = i.SaveString(rows)
+	if err == nil {
+		t.Error("test SaveString fail")
+	}
+
+	// influxdb地址错误
+	config = `{
+    "address":"http://192.168.0.186:8086",
+    "db":"influxdb_test",
+    "user":"admin",
+    "password":"123456",
+    "row":"test_table,name=@name,address=@address age=@age"
+}`
+	i, err = New(config)
+	if err != nil {
+		t.Errorf("create influxdb context fail:%v", err)
+	}
+
+	rows = `[{"name":"champly","address":"china","age":"18"}]`
+	err = i.SaveString(rows)
+	if err == nil {
+		t.Error("test SaveString fail")
+	}
+
+	// 数据库错误
+	config = `{
+    "address":"http://192.168.0.185:8086",
+    "db":"influxdb_test_err",
+    "user":"admin",
+    "password":"123456",
+    "row":"test_table,name=@name,address=@address age=@age"
+}`
+	i, err = New(config)
+	if err != nil {
+		t.Errorf("create influxdb context fail:%v", err)
+	}
+
+	rows = `[{"name":"champly","address":"china","age":"18"}]`
+	err = i.SaveString(rows)
+	if err == nil {
+		t.Error("test SaveString fail")
+	}
+
+	// 参数错误
+	config = `{
+    "address":"http://192.168.0.185:8086",
+    "db":"influxdb_test",
+    "user":"admin",
+    "password":"123456",
+    "row":"test_table,name=@name,address=@address age=@age"
+}`
+	i, err = New(config)
+	if err != nil {
+		t.Errorf("create influxdb context fail:%v", err)
+	}
+
+	rows = `["name":"champly","address":"china","age":"18"}]`
+	err = i.SaveString(rows)
+	if err == nil {
+		t.Error("test SaveString fail")
+	}
+}
