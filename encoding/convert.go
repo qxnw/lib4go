b588b62ffa048ff215cd8d21559f993286f8ca9b
zkfy <lib4net@163.com>
2016-11-28 14:40:27 +0800

    增加base64编码格式

---
+++ encoding/convert.go
@@ -0,0 +1,47 @@
+package encoding
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+)
+
+// GetReader 获取
+// charset不区分大小写
+func GetReader(content string, charset string) io.Reader {
+	charset = strings.ToLower(charset)
+	if strings.EqualFold(charset, "gbk") || strings.EqualFold(charset, "gb2312") {
+		return transform.NewReader(bytes.NewReader([]byte(content)), simplifiedchinese.GBK.NewDecoder())
+	}
+	return strings.NewReader(content)
+
+}
+
+// Convert []byte转换为字符串
+// encoding 支持gbk，gb2312，utf-8	不区分大小写
+func Convert(data []byte, encoding string) (content string, err error) {
+	encoding = strings.ToLower(encoding)
+	if !strings.EqualFold(encoding, "gbk") && !strings.EqualFold(encoding, "gb2312") &&
+		!strings.EqualFold(encoding, "utf-8") {
+		err = fmt.Errorf("不支持的编码方式：%s", encoding)
+		return
+	}
+	//转换utf-8格式
+	if strings.EqualFold(encoding, "utf-8") {
+		content = string(data)
+		return
+	}
+
+	//转换gbk gb2312格式
+	buffer, err := ioutil.ReadAll(transform.NewReader(bytes.NewReader(data), simplifiedchinese.GB18030.NewDecoder()))
+	if err != nil {
+		return
+	}
+	content = string(buffer)
+	return
+}
