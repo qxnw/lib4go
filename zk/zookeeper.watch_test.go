0d0a4dc8a8c5cfcc712757d2976748bea0c5c7c8
zkfy <lib4net@163.com>
2016-12-08 14:36:13 +0800

    .

--- zk/zookeeper.watch_test.go
+++ zk/zookeeper.watch_test.go
@@ -190,7 +190,11 @@ func TestUnbindWatchValue(t *testing.T) {
 	t.Log("释放监控")
 	time.Sleep(time.Second * 1)
 	masterClient.Disconnect()
+<<<<<<< HEAD
+
+=======
 	time.Sleep(time.Second * 1)
+>>>>>>> 49510abf4584799349d29d0d931d1eacecaa8072
 }
 
 // TestBindWatchChildren 测试监控一个节点的子节点
