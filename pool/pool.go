d8dd0a1b6f1c36f407bd0389a38d2bb495ce2d09
zkfy <lib4net@163.com>
2016-12-09 13:27:02 +0800

    增加连接池

---
+++ pool/pool.go
@@ -0,0 +1,142 @@
+package pool
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
+
+//pool 存放链接信息
+type pool struct {
+	mu          sync.Mutex
+	conns       chan *idleConn
+	factory     func() (interface{}, error)
+	close       func(interface{}) error
+	idleTimeout time.Duration
+}
+
+type idleConn struct {
+	conn interface{}
+	t    time.Time
+}
+
+//NewPool 初始化链接
+func NewPool(config *PoolConfigOptions) (IPool, error) {
+	if config.InitialCap < 0 || config.MaxCap <= 0 || config.InitialCap > config.MaxCap {
+		return nil, errors.New("invalid capacity settings")
+	}
+
+	c := &pool{
+		conns:       make(chan *idleConn, config.MaxCap),
+		factory:     config.Factory,
+		close:       config.Close,
+		idleTimeout: config.IdleTimeout,
+	}
+
+	for i := 0; i < config.InitialCap; i++ {
+		conn, err := c.factory()
+		if err != nil {
+			c.Release()
+			return nil, fmt.Errorf("factory is not able to fill the pool: %s", err)
+		}
+		c.conns <- &idleConn{conn: conn, t: time.Now()}
+	}
+
+	return c, nil
+}
+
+//getConns 获取所有连接
+func (c *pool) getConns() chan *idleConn {
+	c.mu.Lock()
+	conns := c.conns
+	c.mu.Unlock()
+	return conns
+}
+
+//Get 从pool中取一个连接
+func (c *pool) Get() (interface{}, error) {
+	conns := c.getConns()
+	if conns == nil {
+		return nil, ErrClosed
+	}
+	for {
+		select {
+		case wrapConn := <-conns:
+			if wrapConn == nil {
+				return nil, ErrClosed
+			}
+			//判断是否超时，超时则丢弃
+			if timeout := c.idleTimeout; timeout > 0 {
+				if wrapConn.t.Add(timeout).Before(time.Now()) {
+					//丢弃并关闭该链接
+					c.Close(wrapConn.conn)
+					continue
+				}
+			}
+			return wrapConn.conn, nil
+		default:
+			conn, err := c.factory()
+			if err != nil {
+				return nil, err
+			}
+
+			return conn, nil
+		}
+	}
+}
+
+//Put 将连接放回pool中
+func (c *pool) Put(conn interface{}) error {
+	if conn == nil {
+		return errors.New("connection is nil. rejecting")
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.conns == nil {
+		return c.Close(conn)
+	}
+
+	select {
+	case c.conns <- &idleConn{conn: conn, t: time.Now()}:
+		return nil
+	default:
+		//连接池已满，直接关闭该链接
+		return c.Close(conn)
+	}
+}
+
+//Close 关闭单条连接
+func (c *pool) Close(conn interface{}) error {
+	if conn == nil {
+		return errors.New("connection is nil. rejecting")
+	}
+	return c.close(conn)
+}
+
+//Release 释放连接池中所有链接
+func (c *pool) Release() {
+	c.mu.Lock()
+	conns := c.conns
+	c.conns = nil
+	c.factory = nil
+	closeFun := c.close
+	c.close = nil
+	c.mu.Unlock()
+
+	if conns == nil {
+		return
+	}
+
+	close(conns)
+	for wrapConn := range conns {
+		closeFun(wrapConn.conn)
+	}
+}
+
+//Len 连接池中已有的连接
+func (c *pool) Len() int {
+	return len(c.getConns())
+}
