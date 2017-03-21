d046f2cd329c54f92bbcdb38b1c241453f29ecbf
zkfy <lib4net@163.com>
2017-03-21 18:34:33 +0800

    .

--- rpc/server/limiter.go
+++ rpc/server/limiter.go
@@ -8,15 +8,26 @@ type limiter struct {
 
 func (m limiter) Handle(ctx *Context) {
 	service := ctx.Req().Service
+	if count, ok := m.data["*"]; ok {
+		conterName := metrics.MakeName(ctx.server.serverName+".limit_", "service", "*")
+		meter := metrics.GetOrRegisterMeter(conterName, metrics.DefaultRegistry)
+		if meter.Rate1() >= count {
+			ctx.Forbidden()
+			ctx.server.logger.Error("service:%s 超过总流程限制(%d)", service, count)
+			return
+		}
+		meter.Mark(1)
+	}
 	if count, ok := m.data[service]; ok {
 		conterName := metrics.MakeName(ctx.server.serverName+".limit_", "service", service)
 		meter := metrics.GetOrRegisterMeter(conterName, metrics.DefaultRegistry)
 		if meter.Rate1() >= count {
 			ctx.Forbidden()
-			ctx.server.logger.Error("service:%s 超过流程限制(%d)", service, count)
+			ctx.server.logger.Error("service:%s 超过当前服务的流程限制(%d)", service, count)
 			return
 		}
 		meter.Mark(1)
 	}
+
 	ctx.Next()
 }
