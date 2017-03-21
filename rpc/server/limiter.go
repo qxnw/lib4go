4be136a240fe42cc17a94a4d4e8c1b9a17e13780
zkfy <lib4net@163.com>
2017-03-21 18:29:27 +0800

    .

---
+++ rpc/server/limiter.go
@@ -0,0 +1,22 @@
+package server
+
+import "github.com/qxnw/lib4go/metrics"
+
+type limiter struct {
+	data map[string]float64
+}
+
+func (m limiter) Handle(ctx *Context) {
+	service := ctx.Req().Service
+	if count, ok := m.data[service]; ok {
+		conterName := metrics.MakeName(ctx.server.serverName+".limit_", "service", service)
+		meter := metrics.GetOrRegisterMeter(conterName, metrics.DefaultRegistry)
+		if meter.Rate1() >= count {
+			ctx.Forbidden()
+			ctx.server.logger.Error("service:%s 超过流程限制(%d)", service, count)
+			return
+		}
+		meter.Mark(1)
+	}
+	ctx.Next()
+}
