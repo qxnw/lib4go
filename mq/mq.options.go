c47ce0c6bb12027b8fb13ce9e1caa95fbee1bd85
zkfy <lib4net@163.com>
2017-04-28 18:24:05 +0800

    fix bugs

---
+++ mq/mq.options.go
@@ -0,0 +1,41 @@
+package mq
+
+import "github.com/qxnw/lib4go/logger"
+
+type option struct {
+	logger     *logger.Logger
+	version    string
+	persistent string
+	ack        string
+}
+
+//Option 配置选项
+type Option func(*option)
+
+//WithLogger 设置日志记录组件
+func WithLogger(logger *logger.Logger) Option {
+	return func(o *option) {
+		o.logger = logger
+	}
+}
+
+//WithVersion 设置版本号
+func WithVersion(version string) Option {
+	return func(o *option) {
+		o.version = version
+	}
+}
+
+//WithPersistent 设置数据模式
+func WithPersistent(persistent string) Option {
+	return func(o *option) {
+		o.persistent = persistent
+	}
+}
+
+//WithAck 设置客户端确认模式
+func WithAck(ack string) Option {
+	return func(o *option) {
+		o.ack = ack
+	}
+}
