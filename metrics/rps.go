c986bcf9879d78433ced3d2e87cd00e1b152d3d2
zkfy <lib4net@163.com>
2017-06-15 17:18:55 +0800

    .

--- metrics/rps.go
+++ metrics/rps.go
@@ -5,10 +5,10 @@ import "sync"
 // RPS count events to produce exponentially-weighted moving average rates
 // at one-, five-, and fifteen-minutes and a mean rate.
 type RPS interface {
-	Mark(int32)
-	M1() int32
-	M5() int32
-	M15() int32
+	Mark(int64)
+	M1() int64
+	M5() int64
+	M15() int64
 }
 
 // GetOrRegisterRps returns an existing Meter or constructs and registers a
@@ -36,19 +36,19 @@ func newStandardRPS() RPS {
 	}
 }
 
-func (s *StandardRPS) Mark(i int32) {
+func (s *StandardRPS) Mark(i int64) {
 	s.lock.Lock()
 	s.m1.Mark(i)
 	s.m5.Mark(i)
 	s.m15.Mark(i)
 	s.lock.Unlock()
 }
-func (s *StandardRPS) M1() int32 {
+func (s *StandardRPS) M1() int64 {
 	return s.m1.counter
 }
-func (s *StandardRPS) M5() int32 {
+func (s *StandardRPS) M5() int64 {
 	return s.m5.counter
 }
-func (s *StandardRPS) M15() int32 {
+func (s *StandardRPS) M15() int64 {
 	return s.m15.counter
 }
